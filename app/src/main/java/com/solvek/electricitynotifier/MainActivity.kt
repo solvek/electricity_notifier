ba60b84b3b0e6435d7092d5d0b4ce85f9ef05030
solvek <adamchuk@gmail.com>
2024-06-10 18:49:26 +0300

    Disable periodic task

--- app/src/main/java/com/solvek/electricitynotifier/MainActivity.kt
+++ app/src/main/java/com/solvek/electricitynotifier/MainActivity.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.solvek.electricitynotifier.EnApp.Companion.enApp
-import com.solvek.electricitynotifier.EnWorker.Companion.schedulePeriodic
 import com.solvek.electricitynotifier.ui.theme.ElectricityNotifierTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        schedulePeriodic()
+//        schedulePeriodic()
         model.log("Scheduled")
         setContent {
             val content by model.log.collectAsState()
